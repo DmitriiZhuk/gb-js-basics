3c1c56864735c61bed4d5e5510fa70b20d30393b
Dmitrii Zhuk <dmitry.v.zhuk@gmail.com>
2023-08-23 10:03:22 +0300

    Home Task 05

---
+++ Home Task 05/2.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/*
+Необходимо из объекта post, вывести значения, к которым приписан комментарий, 
+в консоль.
+*/
+
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+
+// Вывести имя автора
+console.log(post.author);
+
+// Вывести количество dislikes для первого комментария
+console.log(post.comments[0].rating.dislikes);
+
+// Вывести userId для второго комментария
+console.log(post.comments[1].userId);
+
+// Вывести текст второго комментария
+console.log(post.comments[1].text);
